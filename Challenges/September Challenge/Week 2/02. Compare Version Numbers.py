229b409c4c5baf241efc399291d0a793650d4a92
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-09-10 01:31:42 +0530

    Added Code

---
+++ Challenges/September Challenge/Week 2/02. Compare Version Numbers.py
@@ -0,0 +1,32 @@
+
+def check_version(v1,v2):
+    
+    l1 = [int(i) for i in v1.split(".")]
+    l2 = [int(i) for i in v2.split(".")]
+
+    while(len(l1)<len(l2)):
+        l1.append(0)
+
+    while(len(l1)>len(l2)):
+        l2.append(0)
+
+    i=0
+    while i<len(l1) and i<len(l2):
+
+        if l1[i]>l2[i]:
+            return 1
+        elif l1[i]<l2[i]:
+            return -1
+        
+        i+=1
+
+
+    return 0
+
+
+
+v1 = input()
+v2 = input()
+
+print(check_version(v1,v2))
+    
